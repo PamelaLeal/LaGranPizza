5e44a5d5499287d57038ca8238df619fe38cab5a
PamelaViridianaLeal <pamelavlealr@gmail.com>
2020-12-05 14:02:51 -0600

    Prototipo

--- Pizzeria/src/pizzas/Orden.java
+++ Pizzeria/src/pizzas/Orden.java
@@ -11,48 +11,56 @@ public class Orden {
 	
 	
 	public void bienvenida() {
-		//Opciones iniciales
-		System.out.println("Buen día a la pizzería 'La gran pizza' \n ¿Desea realizar?(Por favor indicar el número de la acción)"
-				+ " \n 1.- Ordenar una pizza \n 2.-Ver el menú ");
-		
-		 numero= teclado.nextInt();
-		 teclado.nextLine();//se limpia buffer de entrada
-		 
-		 //condición para saber que opción se ha elegido
-		 if(numero== 1) {
+		do {
+			//Opciones iniciales
+			System.out.println("Buen día a la pizzería 'La gran pizza' \n ¿Desea realizar?(Por favor indicar el número de la acción)"
+					+ " \n 1.- Ordenar una pizza \n 2.-Ver el menú ");
+			
+			 numero= teclado.nextInt();
+			 teclado.nextLine();//se limpia buffer de entrada
 			 
-			 do {
-				 
-				 System.out.println("¿Que desea ordenar? (Ingresar el nombre de la pizza)");
-				 tipoPizza= teclado.nextLine();
-				 
-				 //Condición para saber si se ha ingresado el nombre correcto de la pizza
-				 if(tipoPizza.equalsIgnoreCase("chicago") || 
-						 tipoPizza.equalsIgnoreCase("hawaii") ||
-						 tipoPizza.equalsIgnoreCase("mexicana") ||
-						 tipoPizza.equalsIgnoreCase("clasica")) 
-				 {
-					 //Se manda a llamar el metodo estado de la clase menu, recibiendo como parametro el nombre de la pizza
-					 menu.estado(tipoPizza);
-					 System.out.println("¿Desea ordenar otra vez?");
-					 res= teclado.nextLine();
+			 //condición para saber que opción se ha elegido
+			 if(numero== 1) {
+				 	 
+					 System.out.println("¿Que desea ordenar? (Ingresar el nombre de la pizza)");
+					 tipoPizza= teclado.nextLine();
 					 
-				 }else {
-					 //Si el usuario ingresa mal el nombre de la pizza se mostrará una alerta
-					 System.out.println("No contamos con esa pizza en el menú, revise su orden por favor \n");
-					 res= "si";
-				 }
+					 //Condición para saber si se ha ingresado el nombre correcto de la pizza
+					 if(tipoPizza.equalsIgnoreCase("chicago") || 
+							 tipoPizza.equalsIgnoreCase("hawaii") ||
+							 tipoPizza.equalsIgnoreCase("mexicana") ||
+							 tipoPizza.equalsIgnoreCase("clasica")) 
+					 {
+						 //Se manda a llamar el metodo estado de la clase menu, recibiendo como parametro el nombre de la pizza
+						 menu.estado(tipoPizza);
+						 System.out.println("¿Desea ordenar otra vez?");
+						 res= teclado.nextLine();
+						 
+					 }else {
+						 //Si el usuario ingresa mal el nombre de la pizza se mostrará una alerta
+						 System.out.println("No contamos con esa pizza en el menú, revise su orden por favor \n");
+						 res= "si";
+					 }
+					 
+					 
 				 
-				 //ciclo para repetir la orden si se quiere hacer otra
-			 }while(res.equalsIgnoreCase("si"));
+				
+				 
+				 
+			 }else if(numero== 2) {
+				 //En la opción número 2 se muestra el menú
+				 menu.menu();
+				 System.out.println("¿Desea ordenar?");
+				 res= teclado.nextLine();
+				 
+				 
+			 }
 			 
-			 System.out.println("Gracias por comer aquí, vuelva pronto");
+			//ciclo para repetir la orden si se quiere hacer otra
 			 
-			 
-		 }else if(numero== 2) {
-			 //En la opción número 2 se muestra el menú
-			 menu.menu();
-		 }
+		 }while(res.equalsIgnoreCase("si"));
+		
+		 System.out.println("Gracias por comer aquí, vuelva pronto");
 		
 	}
 	
